1d9c60fd00da457293d195442b73755c03f84fea
E3than <epigou@gmail.com>
2026-06-02 12:42:01 -0400

    account change

--- experiments/Exp_9999/train_job.sh
+++ experiments/Exp_9999/train_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Exp_9999_train
 #SBATCH --output=experiments/Exp_9999/output.log
 #SBATCH --error=experiments/Exp_9999/error.log
-#SBATCH -A rrg-kgroling
+#SBATCH -A def-mcapretz
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=00:05:00
