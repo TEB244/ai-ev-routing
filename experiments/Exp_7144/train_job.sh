d7c8252e44ba0b67eb324bb40587ec76d54c55df
E3than <epigou@gmail.com>
2026-06-04 22:35:28 -0400

    allocation

--- experiments/Exp_7144/train_job.sh
+++ experiments/Exp_7144/train_job.sh
@@ -5,8 +5,8 @@
 #SBATCH -A  rrg-kgroling
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
-#SBATCH --time=34:00:00
-#SBATCH --mem=64G
+#SBATCH --time=10:00:00
+#SBATCH --mem=40G
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3_1g.10gb:1
 
 #SBATCH --mail-type=FAIL,TIME_LIMIT
