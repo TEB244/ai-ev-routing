95529d1812eb10dbcd4560d565a3f37f86948cd5
Santiago Gomez Rosero <sgomezro@uwo.ca>
2026-06-19 13:59:21 -0400

    veryfing before merge

--- drac/parallel_gpu_H100.py
+++ drac/parallel_gpu_H100.py
@@ -42,7 +42,7 @@ config_general = {
 }
 config_odt = {
     "batch_size": 8,
-    "time": "01:00:00", # hours
+    "time": "00:20:00", # hours
     "cpu_per_experiment": 4,
     "mem_per_experiment": 12, # Gigabytes
     "gpus": 1,
