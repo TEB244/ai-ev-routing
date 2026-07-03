bbe0e4629bc9cfd936f9417e936e4eaf9c1efff1
Lucas Hartman <90533367+LucasHartmanWestern@users.noreply.github.com>
2026-07-03 17:04:57 -0400

    Update jobs

--- experiments/Exp_6077/train_job.sh
+++ experiments/Exp_6077/train_job.sh
@@ -5,7 +5,7 @@
 #SBATCH -A def-mcapretz
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
-#SBATCH --time=11:20:00
+#SBATCH --time=22:00:00
 #SBATCH --mem=6G
 #SBATCH --mail-type=FAIL,TIME_LIMIT
 #SBATCH --mail-user=lhartma8@uwo.ca
